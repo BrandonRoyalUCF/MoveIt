1067cb5ff76619fabd35c4ee5eb0166add17e6a0
DavidsonJamesUCF <36898532+DavidsonJamesUCF@users.noreply.github.com>
2018-03-08 15:35:37 -0500

    new pages
    Client and Driver Settings Pages

---
+++ Android/app/src/main/java/com/example/mdo3/overhaul/ClientSettings.java
@@ -0,0 +1,13 @@
+package com.example.mdo3.overhaul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_settings);
+    }
+}
