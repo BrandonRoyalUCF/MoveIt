5f69e90180e9f3f566aa5152784c989aae9efec1
WNSanabria <wnsanabria@knights.ucf.edu>
2018-03-31 13:13:45 -0400

    Finishing Map-related Changes
    When a request is active, the Customer will have a map on their main screen that will allow them to track their driver. It refreshes every 3 minutes.
    
    For the Driver, two extra buttons will appear on their screen. These buttons will open Google Maps to give the Driver directions to either the pick-up or drop-off location, depending on which button they clicked on.
    
    For both pages, the map/buttons will be hidden if a request is not active.

--- Android/app/src/main/java/com/example/mdo3/overhaul/ClientMainScreen.java
+++ Android/app/src/main/java/com/example/mdo3/overhaul/ClientMainScreen.java
@@ -8,30 +8,36 @@ import android.widget.Button;
 import android.content.Intent;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import android.graphics.drawable.Drawable;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-
-import java.io.InputStream;
-import java.net.URL;
+import android.os.Handler;
 
 public class ClientMainScreen extends Activity implements OnMapReadyCallback{
+    // Set this variable based on whether or not a request is active, and thus whether or not to show the driver map.
+    // Ideally this should be set whenever the user moves to this screen, thus the setting for this variable
+    // should be within OnClickListeners that send the user here.
+    public boolean requestActive;
+    // Note that primitive booleans (lowercase B) will be false if left uninitialized.
+
+    private final Handler mapHandler = new Handler(); // For handling the automatic map refresh.
+    private GoogleMap driverMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main_screen);
 
+        requestActive = true; // Remove this later, just for debugging at the moment.
+
         MapFragment mapView = (MapFragment) getFragmentManager().findFragmentById(R.id.map_View);
-        mapView.getMapAsync(this);
+         if (requestActive){
+             mapView.getView().setVisibility(View.VISIBLE);
+             mapView.getMapAsync(this);
+         } else {
+             mapView.getView().setVisibility(View.INVISIBLE);
+         }
 
         //Request Button
         OnClickListener requestListen = new OnClickListener() {
@@ -68,23 +74,42 @@ public class ClientMainScreen extends Activity implements OnMapReadyCallback{
         };
         Button logoutBtn = (Button) findViewById(R.id.button_logout);
         logoutBtn.setOnClickListener(logoutListen);
+
+        mapAutoRefresh();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        /*googleMap.getUiSettings().setScrollGesturesEnabled(false);
-        googleMap.getUiSettings().setZoomGesturesEnabled(false);
-        googleMap.setMapType(1);
-        googleMap.setMaxZoomPreference(12);
-        googleMap.setMinZoomPreference(12); */
+        if (requestActive) {
+            googleMap.setMapType(1);
+            googleMap.setMaxZoomPreference(12);
+            googleMap.setMinZoomPreference(12);
 
-        // TODO: Have the LatLng data set here for driver be set to the driver's location, and refreshed every 3 to 5 minutes.
-        LatLng driver = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(driver).title("Your Driver"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(driver));
+            // TODO: Have the LatLng data set here for driver be set to the driver's location, and refreshed every 3 to 5 minutes.
+            LatLng driver = new LatLng(-33.852, 151.211);
+            googleMap.addMarker(new MarkerOptions().position(driver).title("Your Driver"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(driver));
+
+            driverMap = googleMap;
+        } else {
+            // Do nothing. The map should be hidden while there are no active requests anyways.
+        }
+
     }
 
-    // TODO: Refresh map function
-    // https://stackoverflow.com/questions/17113526/android-auto-refresh-after-a-certain-time
+    // The follow function refreshes the map to keep the customer up to date on the driver's location.
+    private void mapAutoRefresh() {
+        mapHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // TODO: Have the LatLng data set here for driver be set to the driver's location, and refreshed every 3 to 5 minutes.
+                LatLng driver = new LatLng(28.5383, -81.3792);
+                driverMap.clear(); // Get rid of the old marker, otherwise they'll just stack up.
+                driverMap.addMarker(new MarkerOptions().position(driver).title("Your Driver"));
+                driverMap.moveCamera(CameraUpdateFactory.newLatLng(driver));
 
+                mapAutoRefresh(); // Refresh again in a while.
+            }
+        }, 180000); // Refresh every 3 minutes. 180000 milliseconds = 3 minutes.
+    }
 }
--- Android/app/src/main/java/com/example/mdo3/overhaul/DriverMainScreen.java
+++ Android/app/src/main/java/com/example/mdo3/overhaul/DriverMainScreen.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.net.Uri;
 
 /**
  * Created by Wendelyn on 3/25/2018.
  */
 
 public class DriverMainScreen extends Activity{
+    // Set this variable based on whether or not a request is active, and thus whether or not to show the driver map.
+    // Ideally this should be set whenever the user moves to this screen, thus the setting for this variable
+    // should be within OnClickListeners that send the user here.
+    public boolean requestActive;
+    // Note that primitive booleans (lowercase B) will be false if left uninitialized.
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main_screen);
@@ -54,5 +61,45 @@ public class DriverMainScreen extends Activity{
         };
         Button logoutBtn = (Button) findViewById(R.id.button_logout);
         logoutBtn.setOnClickListener(logoutListen);
+
+        requestActive = false; // Remove later, only for debugging right now.
+
+        Button pickUpBtn = (Button) findViewById(R.id.button_pickup);
+        Button dropOffBtn = (Button) findViewById(R.id.button_dropoff);
+        if (requestActive){
+            // Make sure the buttons are visible, and that they send the user to Google Maps
+            // for the pick-up or drop-off.
+            pickUpBtn.setVisibility(View.VISIBLE);
+            View.OnClickListener pickUpListen = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Go to Google Maps with directions to pick-up address.
+                    // TODO: Replace the string in the Uri.encode with whatever string was put in for the request's pick-up location.
+                    Uri gmmIntentUri = Uri.parse("google.navigation:q=" + Uri.encode("University of Central Florida"));
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }
+            };
+            pickUpBtn.setOnClickListener(pickUpListen);
+
+            dropOffBtn.setVisibility(View.VISIBLE);
+            View.OnClickListener dropOffListen = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Go to Google Maps with drop-off address.
+                    // TODO: Replace the string in the Uri.encode with whatever string was put in for the request's drop-off location.
+                    Uri gmmIntentUri = Uri.parse("google.navigation:q=" + Uri.encode("Epcot"));
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    startActivity(mapIntent);
+                }
+            };
+            dropOffBtn.setOnClickListener(dropOffListen);
+        } else {
+            // Hide the buttons and don't give them an OnClick action.
+            pickUpBtn.setVisibility(View.INVISIBLE);
+            dropOffBtn.setVisibility(View.INVISIBLE);
+        }
     }
 }
