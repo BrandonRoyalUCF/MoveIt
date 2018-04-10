14d25959ef18e8b10ea4f3e21e859fe3f65a78c2
Popdog <wmyers09@gmail.com>
2018-04-10 13:51:35 -0400

    Accept and Decline navigation

--- Android/app/src/main/java/com/example/mdo3/overhaul/DriverRequestScreen.java
+++ Android/app/src/main/java/com/example/mdo3/overhaul/DriverRequestScreen.java
@@ -1,7 +1,10 @@
 package com.example.mdo3.overhaul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DriverRequestScreen extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class DriverRequestScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_request_screen);
+
+        //Decline Button
+        View.OnClickListener declineListen = new View.OnClickListener() {
+            @Override
+            // Need to eventually have it actually save data to the database
+            public void onClick(View v) {
+                finish();
+                Intent myIntent = new Intent(DriverRequestScreen.this, DriverMainScreen.class);
+                DriverRequestScreen.this.startActivity(myIntent);
+            }
+        };
+        Button declineBtn = (Button) findViewById(R.id.button_decline);
+        declineBtn.setOnClickListener(declineListen);
+
+        //Accept Button
+        View.OnClickListener acceptListen = new View.OnClickListener() {
+            @Override
+            // Need to eventually have it actually save data to the database
+            public void onClick(View v) {
+                finish();
+                Intent myIntent = new Intent(DriverRequestScreen.this, DriverMainScreen.class);
+                DriverRequestScreen.this.startActivity(myIntent);
+            }
+        };
+        Button acceptBtn = (Button) findViewById(R.id.button_accept);
+        acceptBtn.setOnClickListener(acceptListen);
     }
 }
